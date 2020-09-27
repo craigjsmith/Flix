ff28e047bc1b02c70fa8ac7c66d8501ed776a166
Craig Smith <craigjsmith99@gmail.com>
2020-09-27 16:33:18 -0400

    Initial commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Craig Smith on 9/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
