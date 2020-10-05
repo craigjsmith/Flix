6a77e26bc8dfc9b39cd070f7c65eccc2a267ff00
Craig Smith <craigjsmith99@gmail.com>
2020-10-04 21:49:04 -0400

    Unit 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Craig Smith on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
